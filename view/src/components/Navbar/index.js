02a8ee5d43010344ddbffcd1640089c71c3d4d90
slawomir.krzyzak@gmail.com <slawomir.krzyzak@gmail.com>
2022-05-06 20:16:29 +0200

    mixins styles for all

--- view/src/components/Navbar/index.js
+++ view/src/components/Navbar/index.js
@@ -1,12 +1,19 @@
 import {Link} from "react-router-dom";
 const Navbar=()=>{
     return(
-        <div>
+        <div className="navbar">
             <nav>
-                <Link className="mr-4 text-amber-800" to="/">Home</Link>
-                <Link className="mr-4 text-amber-800" to="houses">Houses</Link>
-                <Link className="mr-4 text-amber-800" to="house">House</Link>
+                <Link className="link m-l16 mr-4 text-amber-800" to="/"> <p className="h3Search"> Home</p></Link>
+                                <div className="vl"></div>
+
+                <Link className="link m-l16 mr-4 text-amber-800" to="houses"><p className="h3Search">Houses </p></Link>
+                                <div className="vl"></div>
+
+                <Link className="link m-l16 mr-4 text-amber-800" to="house"><p className="h3Search" >House </p></Link>
+                <div className="vl"></div>
+                <Link className="link m-l16 mr-4 text-amber-800" to="guests"><p className="h3Search"> Guests</p></Link>
             </nav>
+
         </div>
     )
 }
