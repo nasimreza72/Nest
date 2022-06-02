0992fb797091e3fc140ee85d57b0aac60696a9dd
Ronny <ronnykorneli@googlemail.com>
2022-06-02 16:03:33 +0200

    Made some changes to HOME pages
    Still need to du stuff, but getting there

--- src/pages/Home/Main/index.jsx
+++ src/pages/Home/Main/index.jsx
@@ -8,72 +8,87 @@ export const Main=()=>{
             <div className="grid-container">
 
                 <div className="blackBox"></div>
-                <div className="refugeeBanner">
-                    <h1>Help house 100,000 refugees fleeing Ukraine</h1>
-                    <button>Learn More</button>
-                </div>
+               
                 <div className="firstLargeImage">
-                    <h2>Let your curiosity do the booking</h2>
-                    <button>I`m flexible</button>
+                    <h2>NEST- We provide housing for refugees</h2>
+                    <button>Browse housing</button>
                 </div>
 
-                <h2>Inspiration for your next trip</h2>
+                <h2>Locations with available housing</h2>
                 
-                <div className="inspirationOne"></div>
+                <div className="locationOne"></div>
                 <div className="locations">
                     <h3>Berlin</h3>
-                    <p>0 kilometets away</p>
+                    <p>123 Apartements</p>
                 </div>
 
-                <div className="inspirationTwo"></div>
+                <div className="locationTwo"></div>
                 <div className="locations">
                     <h3>Rostock</h3>
-                    <p> 195 kilometets away</p>
+                    <p>5 Apartements</p>
                 </div>
 
-                <div className="inspirationThree"></div>
+                <div className="locationThree"></div>
                 <div className="locations">
                     <h3>Hamburg</h3>
-                    <p>255 kilometets away</p>
+                    <p>78 Apartements</p>
                 </div>
 
-                <div className="inspirationFour"></div>
-                <div className="locations">
+                <div className="locationFour"></div>
+                <div className="locations"> 
                     <h3>Usedom</h3>
-                    <p>155 kilometets away</p>
+                    <p>1 Apartement</p>
                 </div>
 
-                <h2>Discover Airbnb Experiences</h2>
+                <h2>What is NEST?</h2>
 
-                <div className="doOnYourTripImage"></div>
 
-                <div className="filterDoTripStuff doStuff">
+                <div className="about-us">
                     <div>
-                        <h3>Things to do</h3>
-                        <h3 id="secondHThree">on your trip</h3>
-                        <button>Experiences</button>
+                        <h3>About us</h3>
                     </div>
+                        <p>
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                           Impedit eligendi fugiat eius quod possimus, 
+                           illum expedita eum sint aspernatur labore suscipit 
+                           nesciunt quaerat libero facilis iure. 
+                           Repellat neque ea rem! Lorem ipsum dolor sit 
+                           amet, consectetur adipisicing elit. Quidem dicta adipisci nostrum, 
+                           laboriosam repellat hic. Fuga minima nesciunt facilis reiciendis ab sit, dolor quis odio ducimus, 
+                           aliquam cum doloribus nihil?
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                           Impedit eligendi fugiat eius quod possimus, 
+                           illum expedita eum sint aspernatur labore suscipit 
+                           nesciunt quaerat libero facilis iure. 
+                           Repellat neque ea rem! Lorem ipsum dolor sit 
+                           amet, consectetur adipisicing elit. Quidem dicta adipisci nostrum, 
+                           laboriosam repellat hic. Fuga minima nesciunt facilis reiciendis ab sit, dolor quis odio ducimus, 
+                           aliquam cum doloribus nihil?
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                         
+                        </p>
+                    
+                    <button>The Press</button>
                 </div>
                 
-                <div className="doHomeImage doStuff"></div>
-                <div className="filterDoHomeStuff doStuff">
-                    <div>
-                        <h3>Things to do</h3>
-                        <h3 id="secondHThree">from home</h3>
-                        <button>Online Experiences</button>
-                    </div>
-                </div>
+                <div className="imageAboutUs"></div>
 
-                <div className="giftCardsText">
-                    <h2>Shop Airbnb</h2>
-                    <h2 id="secondHTwo">gift cards</h2>
+                <div className="InfoUponEnter">
+                    <h2>Important Info upon </h2>
+                    <h2>entering Germany</h2>
                     <button>Learn more</button>
                 </div>
                 
 
-                <div className="cardOne"></div>
-                <div className="cardTwo"></div>
-                <div className="cardThree"></div>
+                <div className="cardOne cards">
+                    <h3>SOME AMT</h3>
+                </div>
+                <div className="cardTwo cards">
+                    <h3>SOME AMT</h3>
+                </div>
+                <div className="cardThree cards">
+                    <h3>SOME AMT</h3>
+                </div>
 
                 <div className="askSuperHost"></div>
                 <div className="filterSuperHost">
@@ -83,6 +98,9 @@ export const Main=()=>{
                         <h2>hosting?</h2>
                     </div>
                     <button>Ask a Superhost</button>
+                </div>
+                <div className="footer">
+                    <h3>FOOTER</h3>
                 </div>
                 
 
