ecd84c2f049c6784072fb58700d8930cc0a6b6c3
Ömer Iliski <omeriliski06@gmail.com>
2022-06-02 10:34:41 +0200

    added mixin

--- src/components/House/Button/Button.jsx
+++ src/components/House/Button/Button.jsx
@@ -1,7 +1,8 @@
+import "./Button.scss";
 export const Button = ({text})=>{
     return (
         <div className=" flex justify-start mb-10 mt-5">
-            <input className=" border-2 px-20 py-4 rounded-lg hover:bg-gray-100 hover:underline cursor-pointer text-lg" type="button" value={text}/>
+            <input type="button" value={text}/>
         </div>
     )
 }
