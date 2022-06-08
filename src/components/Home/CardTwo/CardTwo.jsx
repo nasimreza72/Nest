42feea6eeba9bfb790b2f07a8ca83e72aeaf61fd
Ronny <ronnykorneli@googlemail.com>
2022-06-08 13:26:29 +0200

    Almost done with the Template for the Home pages
    still have a bug in the carousell, that needs fixing.

---
+++ src/components/Home/CardTwo/CardTwo.jsx
@@ -0,0 +1,10 @@
+export default function CardTwo(){
+
+    return(
+        <div className="CardTwo">
+            <h3>SOME AMT</h3>
+        </div>
+    )
+
+
+}
---
+++ src/components/Home/InfoUponEntering/InfoUponEntering.jsx
@@ -0,0 +1,17 @@
+import "./InfoUponEntering.scss"
+
+
+export default function InfoUponEntering(){
+
+
+    return(
+        <div className="InfoUponEntering">
+            <h2>Important Info upon </h2>
+            <h2>entering Germany</h2>
+            <button>Learn More</button>
+        </div>
+    )
+
+
+
+}
