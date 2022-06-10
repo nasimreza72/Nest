c7cda22c84647a37eea75d0392d65e92fae096d3
Reza <nasimreza72>
2022-06-10 17:00:34 +0200

    added hosting page 3

--- src/components/General Components/Navbar/index.js
+++ src/components/General Components/Navbar/index.js
@@ -1,47 +1,86 @@
-
 import { useContext, useState } from "react";
-import { loginContext } from "../../../Context/LoginContext.jsx"
+import { loginContext } from "../../../Context/LoginContext.jsx";
 import { Link } from "react-router-dom";
 import { BiSearch } from "react-icons/bi";
 import "./index.scss";
-import {Offcanvas, Button} from "react-bootstrap";
-
+import { Offcanvas, Button } from "react-bootstrap";
 
 // const MainNav = ({setIsMainActive}) => {
 //   return (
-    
+
 //   );
 // };
 
-const BigNav=()=>{
-    const [active, setActive] = useState(null);
-    
-    return(
-        <div className="big-nav">
-            <div className="section1">
-                <button>Stays</button>
-                <button>Experiences</button>
-                <button>Online Experiences</button>
-            </div>
-            <div className="section2">
-                <div style={active==0 ? {boxShadow:"0px 0px 10px 4px #DBDBDB", backgroundColor:"white"} :null}>
-                    <h6>Where</h6>
-                    <input onClick={()=>setActive(0)} style={active==0 ? {backgroundColor:"white"}:null} type="text" placeholder="Search destinations"/>
-                </div>
-                <div style={active==1 ? {boxShadow:"0px 0px 10px 4px #DBDBDB", backgroundColor:"white"} :null}>
-                    <h6>When</h6>
-                    <input onClick={()=>setActive(1)} style={active==1 ? {backgroundColor:"white"}:null} type="text" placeholder="Any week"/>
-                </div>
-                <div style={active==2 ? {boxShadow:"0px 0px 10px 4px #DBDBDB", backgroundColor:"white"} :null}>
-                    <h6>Who</h6>
-                    <input onClick={()=>setActive(2)} style={active==2 ? {backgroundColor:"white"}:null} type="text" placeholder="Add guests"/>
-                </div>
-                <div></div>
-            </div>
-          </div>
-    )
-  }
+const BigNav = () => {
+  const [active, setActive] = useState(null);
 
+  return (
+    <div className="big-nav">
+      <div className="section1">
+        <button>Stays</button>
+        <button>Experiences</button>
+        <button>Online Experiences</button>
+      </div>
+      <div className="section2">
+        <div
+          style={
+            active == 0
+              ? {
+                  boxShadow: "0px 0px 10px 4px #DBDBDB",
+                  backgroundColor: "white",
+                }
+              : null
+          }
+        >
+          <h6>Where</h6>
+          <input
+            onClick={() => setActive(0)}
+            style={active == 0 ? { backgroundColor: "white" } : null}
+            type="text"
+            placeholder="Search destinations"
+          />
+        </div>
+        <div
+          style={
+            active == 1
+              ? {
+                  boxShadow: "0px 0px 10px 4px #DBDBDB",
+                  backgroundColor: "white",
+                }
+              : null
+          }
+        >
+          <h6>When</h6>
+          <input
+            onClick={() => setActive(1)}
+            style={active == 1 ? { backgroundColor: "white" } : null}
+            type="text"
+            placeholder="Any week"
+          />
+        </div>
+        <div
+          style={
+            active == 2
+              ? {
+                  boxShadow: "0px 0px 10px 4px #DBDBDB",
+                  backgroundColor: "white",
+                }
+              : null
+          }
+        >
+          <h6>Who</h6>
+          <input
+            onClick={() => setActive(2)}
+            style={active == 2 ? { backgroundColor: "white" } : null}
+            type="text"
+            placeholder="Add guests"
+          />
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
 
 function OffCanvasExample({ name, ...props }) {
   const [show, setShow] = useState(false);
@@ -55,21 +94,22 @@ function OffCanvasExample({ name, ...props }) {
         {name}
       </Button> */}
 
-    <div className="main-nav">
-      <button onClick={handleShow}>Anywhere</button>
-      <button onClick={handleShow}>Any week</button>
-      <button onClick={handleShow}>Add guests</button>
-      <button className="search-button"><BiSearch/></button>
-    </div>
- 
-    <Offcanvas show={show}  onHide={handleClose} {...props}>
-      {/* <Offcanvas.Header>
-      </Offcanvas.Header> */}
-      <Offcanvas.Body className="off-canvas-body">
-        <BigNav/>
-      </Offcanvas.Body>
-    </Offcanvas>
+      <div className="main-nav">
+        <button onClick={handleShow}>Anywhere</button>
+        <button onClick={handleShow}>Any week</button>
+        <button onClick={handleShow}>Add guests</button>
+        <button className="search-button">
+          <BiSearch />
+        </button>
+      </div>
 
+      <Offcanvas show={show} onHide={handleClose} {...props}>
+        {/* <Offcanvas.Header>
+      </Offcanvas.Header> */}
+        <Offcanvas.Body className="off-canvas-body">
+          <BigNav />
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 }
@@ -78,33 +118,46 @@ function Example() {
   return (
     <>
       {/* {['start', 'end', 'top', 'bottom'].map((placement, idx) => ( */}
-        <OffCanvasExample placement="top" name="top" />
+      <OffCanvasExample placement="top" name="top" />
       {/* ))} */}
     </>
   );
 }
 
-
-const Navbar=()=>{
-
-  const {login, setLogin, register, setRegister} = useContext(loginContext);
+const Navbar = () => {
+  const { login, setLogin, register, setRegister } = useContext(loginContext);
   const [isMainActive, setIsMainActive] = useState(true);
 
-  console.log(login)
+  console.log(login);
 
-  return(
-      <div>
-          <nav>
-              <Link className="mr-4 text-amber-800" to="/">Home</Link>
-              <Link className="mr-4 text-amber-800" to="houses">Houses</Link>
-              <Link className="mr-4 text-amber-800" to="house">House</Link>
-              <Link className="mr-4 text-amber-800" to="hostingPage1">HostingPage1</Link>
-              <Link className="mr-4 text-amber-800" to="messages">Messages</Link>
-          </nav>
-          <button onClick={ e => setLogin(!login)}>Login</button>
-          <button onClick={ e => setRegister(!register)}>Register</button>
-
-      </div>
-    )
-  }
+  return (
+    <div>
+      <nav>
+        <Link className="mr-4 text-amber-800" to="/">
+          Home
+        </Link>
+        <Link className="mr-4 text-amber-800" to="houses">
+          Houses
+        </Link>
+        <Link className="mr-4 text-amber-800" to="house">
+          House
+        </Link>
+        <Link className="mr-4 text-amber-800" to="hostingPage1">
+          HP1
+        </Link>
+        <Link className="mr-4 text-amber-800" to="hostingPage2">
+          HP2
+        </Link>{" "}
+        <Link className="mr-4 text-amber-800" to="hostingPage3">
+          HP3
+        </Link>
+        <Link className="mr-4 text-amber-800" to="messages">
+          Messages
+        </Link>
+      </nav>
+      <button onClick={(e) => setLogin(!login)}>Login</button>
+      <button onClick={(e) => setRegister(!register)}>Register</button>
+    </div>
+  );
+};
 export default Navbar;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,12 @@ import Home from "./Home";
 import Houses from "./Houses/Houses";
 import House from "./House";
 import Login from "./Login/Login.jsx"
-import HostingPage1 from "./Hosting/HostingPage1.jsx"
+import HostingPage1 from "./Hosting/HostingPage1/HostingPage1.jsx"
+import HostingPage2 from "./Hosting/HostingPage2/HostingPage2.jsx"
+import HostingPage3 from "./Hosting/HostingPage3/HostingPage3.jsx"
 
 
-export {Home,Houses,House,Login,HostingPage1 };
+export {Home,Houses,House,Login,HostingPage1,HostingPage2, HostingPage3};
 
 
 
