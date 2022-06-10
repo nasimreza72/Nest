1af7dd4007e160e63289167b2ece9717b4abf314
Reza <nasimreza72>
2022-06-10 09:36:39 +0200

    added hostin page bottom part

--- src/pages/Hosting/HostingPage1.jsx
+++ src/pages/Hosting/HostingPage1.jsx
@@ -11,14 +11,14 @@ export default function HostingPage1() {
       <div className="mainRight">
           <div className="subMainRight">
               <div className="subMainRightDiv">
-                  <p>Whole Apartment</p>
+                  <p>Apartment</p>
                   <div className="apartmentImage">
                       <img src="https://a0.muscache.com/im/pictures/eadbcbdb-d57d-44d9-9a76-665a7a4d1cd7.jpg?im_w=240" alt="" width={60} />
                   </div>
               </div>
 
               <div className="subMainRightDiv">
-                  <p>Whole House</p>
+                  <p>House</p>
                   <div className="apartmentImage">
                       <img src="https://a0.muscache.com/im/pictures/d1af74db-58eb-46bf-b3f5-e42b6c9892db.jpg?im_w=240" alt="" width={60} />
                   </div>
@@ -35,6 +35,30 @@ export default function HostingPage1() {
                   <p>Shared Room</p>
                   <div className="apartmentImage">
                       <img src="https://a0.muscache.com/im/pictures/a2c9ad21-b159-4fd2-b417-d810fb23c6a9.jpg?im_w=240" alt="" width={60} />
+                  </div>
+              </div>
+
+              <div className="subMainRightDiv">
+                  <p>Attic</p>
+                  <div className="apartmentImage">
+                      <img src="https://cdnb.artstation.com/p/assets/images/images/038/712/203/large/selami-bektas-attic-bedroom-01.jpg?1623851279" alt="" width={60} />
+                  </div>
+              </div>
+
+              <div className="subMainRightDiv">
+                  <p>Garage</p>
+                  <div className="apartmentImage">
+                      <img src="https://stylebyemilyhenderson.com/wp-content/uploads/2020/06/Emily-Henderson_Shanty-Wijaya_Opener.jpg" alt="" width={60} />
+                  </div>
+              </div>
+          </div>
+          <div className="subMainRightBottom">
+              <div className="bottomPart">
+                  <div className="back">
+                      <button><u>Back</u></button>
+                  </div>
+                  <div className="next">
+                      <button>Next</button>
                   </div>
               </div>
           </div>
