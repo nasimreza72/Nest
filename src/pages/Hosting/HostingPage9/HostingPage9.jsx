45fe69a477bd0a45b13b0b75052770ab9d3610ef
Reza <nasimreza72>
2022-06-13 08:59:26 +0200

    updated hosting page 9

--- src/pages/Hosting/HostingPage9/HostingPage9.jsx
+++ src/pages/Hosting/HostingPage9/HostingPage9.jsx
@@ -1,47 +1,36 @@
 import "./hostingPage9.scss";
 import { IoMdPhotos } from "react-icons/io";
 
-
 export default function HostingPage9() {
-
-
   return (
     <div className="hostingPage9">
       <div className="mainLeft">
-        <h1>Next, let's add some photos of your place</h1>
+        <h1>Check out your listing!</h1>
+        <p>
+          Once you save, we'll ask yo to confirm a few details before you can
+          publish.
+        </p>
       </div>
       <div className="mainRight">
-          <div className="subMainRight">
-              <div className="subMainRightDiv">
-
-                    <div className="uploadImageTop">
-                        <IoMdPhotos className="logo"/>
-                        <div className="dragPhoto">
-                            Drag your photo here
-                        </div>
-                        <div className="addPhoto">
-                            Add at least 5 photos
-                        </div>
-                    </div>
-                    <div className="uploadImageBottom">
-                        <button className="uploadFromDevice">
-                            Upload from your device
-                        </button>
-                    </div>
-
-              </div>
-        
+        <div className="subMainRight">
+          <div className="subMainRightDiv">
+            <h2> All informations </h2>
+            <h5> Summary of all informations</h5>
+            <div className="allInfoList"></div>
           </div>
-          <div className="subMainRightBottom">
-              <div className="bottomPart">
-                  <div className="back">
-                      <button><u>Back</u></button>
-                  </div>
-                  <div className="next">
-                      <button>Next</button>
-                  </div>
-              </div>
+        </div>
+        <div className="subMainRightBottom">
+          <div className="bottomPart">
+            <div className="back">
+              <button>
+                <u>Back</u>
+              </button>
+            </div>
+            <div className="next">
+              <button>Next</button>
+            </div>
           </div>
+        </div>
       </div>
     </div>
   );
