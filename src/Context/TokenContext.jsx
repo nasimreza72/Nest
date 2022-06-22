f1ec15c93b3146922c961cb0d1ab77077ec95efa
Ronny <ronnykorneli@googlemail.com>
2022-06-22 10:09:46 +0200

    CREATED TOKEN Context
        - imported TokenContextProvider from context File into index
        - wrapped teh APP.jsx into it
        - Created the TokenContext File

---
+++ src/Context/TokenContext.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from 'react'
+
+export const tokenContext = React.createContext()
+
+export default function TokenContextProvider(props){
+
+    const [token, setToken] = useState("")
+    const tokenVariable = { token, setToken }
+
+    return(
+        <tokenContext.Provider value={tokenVariable}>
+            {props.children}
+        </tokenContext.Provider>
+    )
+
+}
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import LoginContextProvider from "./Context/LoginContext.jsx";
+import TokenContextProvider from './Context/TokenContext.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
       <LoginContextProvider>
-        <App />
+        <TokenContextProvider>
+          <App />
+        </TokenContextProvider>
       </LoginContextProvider>  
     </BrowserRouter>
 );
