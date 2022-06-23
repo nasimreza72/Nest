34b08208561e7337acef68006e3eaea98fbb9fc8
Reza <nasimreza72>
2022-06-23 18:38:14 +0200

    added current location function

--- src/components/HousesComponents/MapContainer.jsx
+++ src/components/HousesComponents/MapContainer.jsx
@@ -17,48 +17,48 @@ const MapContainer = () => {
   };
 
   const defaultCenter = {
-    lat: 41.3851,
-    lng: 2.1734,
+    lat: 52.52,
+    lng: 13.405,
   };
   const locations = [
     {
       name: "Location 1",
       img: "https://media.istockphoto.com/photos/europe-modern-complex-of-residential-buildings-picture-id1165384568?k=20&m=1165384568&s=612x612&w=0&h=CAnAr3uJtmkr0IQ2EPgm0IBoo8oCm-FEYEtyor8X_9I=",
       location: {
-        lat: 41.3954,
-        lng: 2.162,
+        lat: 52.4954,
+        lng: 13.462,
       },
     },
     {
       name: "Location 2",
       img: "https://images.unsplash.com/photo-1558981082-c7d43331cd8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmxhdHN8ZW58MHx8MHx8&w=1000&q=80",
       location: {
-        lat: 41.3917,
-        lng: 2.1649,
+        lat: 52.4754,
+        lng: 13.412,
       },
     },
     {
       name: "Location 3",
       img: "https://images.unsplash.com/photo-1558981082-c7d43331cd8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmxhdHN8ZW58MHx8MHx8&w=1000&q=80",
       location: {
-        lat: 41.3773,
-        lng: 2.1585,
+        lat: 52.4914,
+        lng: 13.362,
       },
     },
     {
       name: "Location 4",
       img: "https://media.istockphoto.com/photos/europe-modern-complex-of-residential-buildings-picture-id1165384568?k=20&m=1165384568&s=612x612&w=0&h=CAnAr3uJtmkr0IQ2EPgm0IBoo8oCm-FEYEtyor8X_9I=",
       location: {
-        lat: 41.3797,
-        lng: 2.1682,
+        lat: 52.5297,
+        lng: 13.2882,
       },
     },
     {
       name: "Location 5",
       img: "https://images.unsplash.com/photo-1558981082-c7d43331cd8c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZmxhdHN8ZW58MHx8MHx8&w=1000&q=80",
       location: {
-        lat: 41.4055,
-        lng: 2.1915,
+        lat: 52.5055,
+        lng: 13.3915,
       },
     },
   ];
@@ -69,35 +69,42 @@ const MapContainer = () => {
 
   /////// GEO CODE
 
-  Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY);
+  // Geocode.setApiKey(process.env.REACT_APP_GOOGLE_API_KEY);
 
-  Geocode.setLanguage("en");
-  Geocode.setLocationType("ROOFTOP");
-  Geocode.enableDebug();
+  // Geocode.setLanguage("en");
+  // Geocode.setLocationType("ROOFTOP");
+  // Geocode.enableDebug();
 
+  // navigator.geolocation.getCurrentPosition((position) => {
+  //   Geocode.fromLatLng(
+  //     position.coords.latitude,
+  //     position.coords.longitude
+  //   ).then(
+  //     (response) => {
+  //       const address = response.results[0].formatted_address;
+  //       console.log("Current location address-->",address);
+  //     },
+  //     (error) => {
+  //       console.error(error);
+  //     }
+  //   );
+  // });
 
-  navigator.geolocation.getCurrentPosition((position) => {
-    Geocode.fromLatLng(position.coords.latitude, position.coords.longitude).then(
-      (response) => {
-        const address = response.results[0].formatted_address;
-        console.log(address);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  })
+  // Get latitude & longitude from address.
 
-
-
-
-
+  // Geocode.fromAddress("Soldiner Str. 36, 13359 Berlin, Germany").then(
+  //   (response) => {
+  //     const { lat, lng } = response.results[0].geometry.location;
+  //     console.log("Currenr location lat & lng -->", lat, lng);
+  //   },
+  //   (error) => {
+  //     console.error(error);
+  //   }
+  // );
 
   return (
-    <LoadScript 
-    googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}
-    >
-      <GoogleMap mapContainerStyle={mapStyles} zoom={13} center={defaultCenter}>
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY}>
+      <GoogleMap mapContainerStyle={mapStyles} zoom={12} center={defaultCenter}>
         {locations.map((item) => {
           return (
             <Marker
@@ -116,8 +123,8 @@ const MapContainer = () => {
           >
             {/* <p>{selected.name}</p> */}
             <div>
-            <img src={selected.img }alt="" width={100} />
-            <p>{selected.name}</p>
+              <img src={selected.img} alt="" width={100} />
+              <p>{selected.name}</p>
             </div>
           </InfoWindow>
         )}
