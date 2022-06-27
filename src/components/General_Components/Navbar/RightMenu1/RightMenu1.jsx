6b8d40f1619c89aeb936cb04806eba4d620da549
Ronny <ronnykorneli@googlemail.com>
2022-06-27 12:29:41 +0200

    Context Variables fro Login
            Navbar.jsx
                - made active user NULL
    
            RightMenu1.jsx
    
                - Changed the login and setLogin Context Varaiable to
                "loginModal" and "setLoginModal"
    
                - Changed theese variable-names in the rest of the code
    
            LoginContext.jsx
    
                - Created the variables; "submitLoginDetails" and
                "setSubmitLoginDetails"
    
                - I did this this so that the Continue button on Login.jsx
                can trigger the useEffect inside thre Input.jsx.
                The useEffect triggers the Post request to the login backend
    
            Login.jsx
    
                - imported theese context variables;
                "submitLoginDetails" and "setSubmitLoginDetails"
    
                -attached these to the Continue Button
    
            Input.jsx
    
                -  imported theese context variables;
                "submitLoginDetails" and "setSubmitLoginDetails"
    
                - imported axios
    
                - Created a useEffect function to make a post request to
                the Login Backend. This endpoint should recieve the Token

--- src/components/General_Components/Navbar/RightMenu1/RightMenu1.jsx
+++ src/components/General_Components/Navbar/RightMenu1/RightMenu1.jsx
@@ -3,8 +3,8 @@ import { loginContext } from "../../../../Context/LoginContext.jsx";
 import { navbarContext } from "../../../../Context/NavbarContext.jsx";
 import "./RightMenu1.scss";
 
-const RightMenu1 = ()=>{
-    const {login, setLogin, register, setRegister} = useContext(loginContext);
+const RightMenu1 = () => {
+    const {loginModal, setLoginModal, register, setRegister} = useContext(loginContext);
     const {setRightMenu} = useContext(navbarContext);
 
     const rightMenu1 = useRef();
@@ -15,10 +15,12 @@ const RightMenu1 = ()=>{
               setRightMenu(false);
         })
       }, [])
+
+      console.log("login-----------------------" + loginModal)
     return(
         <div ref={rightMenu1} className="right-menu1-container">
             <ul>
-                <li onClick={ e => { setLogin(!login); setRightMenu(false) }}>Log in</li>
+                <li onClick={ e => { setLoginModal(!loginModal); setRightMenu(false) }}>Log in</li>
                 <li onClick={ e => { setRegister(!register); setRightMenu(false)}}>Register</li>
             </ul>
         </div>
