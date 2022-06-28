1d5e66920e045440d8a8d48ff58b032ae4850d51
Ronny <ronnykorneli@googlemail.com>
2022-06-28 11:56:12 +0200

    REGISTER FULLY CONNECTED WITH Backend
        - Only thing that needs to be done is to to add
        the option to choose Host/User on the frontend
    
            WHAT I DID:
                - Created the component; InputRegister.jsx
                - The Component takes two inputs (email and Password)
                  and and sends them to the backend
                - I needed the Context variables;
                submitRegistrationDetails & setSubmitRegistrationDetails
                because the submit handler is triggered by a button on the
                page; Register.jsx. The Button has the
                className of; ContinueButton

--- src/Context/LoginContext.jsx
+++ src/Context/LoginContext.jsx
@@ -9,9 +9,11 @@ export default function LoginContextProvider(props){
     const [loginModal, setLoginModal] = useState(false)
     const [register, setRegister] = useState(false)
     const [submitLoginDetails, setSubmitLoginDetails] = useState(false)
+    const [submitRegistrationDetails, setSubmitRegistrationDetails] = useState(false)
 
     const loginVariable = { loginModal, setLoginModal, register, setRegister,
-                            submitLoginDetails, setSubmitLoginDetails }
+                            submitLoginDetails, setSubmitLoginDetails,
+                            submitRegistrationDetails, setSubmitRegistrationDetails }
 
     return(
         <loginContext.Provider value={loginVariable}>
