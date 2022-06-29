4179268e046019e69c9beae184fb1d6d96401f8d
Ronny <ronnykorneli@googlemail.com>
2022-06-29 10:57:04 +0200

    cleaned code styling

--- src/components/Login/Input.jsx
+++ src/components/Login/Input.jsx
@@ -1,19 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import "./LoginComponent.scss"
 import { loginContext } from "../../Context/LoginContext.jsx"
+import axios from "axios"
 // import { tokenContext } from '../../Context/TokenContext'
 
 // "email":"ponni@hof",
 // "password":"123RonRon!!!"
 
 export default function Input(){
-    const {setEmail, setPassword, email, password } = useContext(loginContext)
+    const {setEmail, setPassword, email, password,  } = useContext(loginContext)
     // const { token, setToken } = useContext(tokenContext)
     // console.log("this is token" + token)
     
     
 
-    console.log("this is token" + token)
+   /*  console.log("this is token" + token)
 
     useEffect(() => {
         console.log("axios")
@@ -44,7 +45,7 @@ export default function Input(){
 
 
   }, [token])
-
+ */
 
     return(
         <form >
