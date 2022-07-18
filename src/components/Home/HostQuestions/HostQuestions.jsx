3221354ce474b7210ac7500878d5c942ca06f4d4
Ronny <ronnykorneli@googlemail.com>
2022-07-18 11:52:27 +0200

    New title on bottom iMage

--- src/components/Home/HostQuestions/HostQuestions.jsx
+++ src/components/Home/HostQuestions/HostQuestions.jsx
@@ -5,9 +5,9 @@ export default function HostQuestions(){
     return(
         <div className="HostQuestions">
             <div className="text">
-                <h2>Questions</h2>
-                <h2 id="adjustPosition">about</h2>
-                <h2 id="adjustPosition">hosting?</h2>
+                <h2>Talk to</h2>
+                <h2 id="adjustPosition">local </h2>
+                <h2 id="adjustPosition">buddy</h2>
             </div>
         </div>
     )
